8ec9d7a318a92733a7c9c68421d3251ef966ba9c
San <18814184179@163.com>
2018-06-18 09:06:27 +0800

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @desc  : Created by San on 2018-06-18 08:59
+# @site  : https://github.com/SunmoonSan
+
---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @desc  : Created by San on 2018-06-18 09:01
+# @site  : https://github.com/SunmoonSan
+from app.app import app
+
+
+if __name__ == '__main__':
+    app.run()
+
